cf87021c92d4412a48d6bb692072190f41ab96c7
doowan <rlaendhks11@gmail.com>
2021-11-02 00:13:26 +0900

    initial settings

--- src/components/bio.js
+++ src/components/bio.js
@@ -29,6 +29,7 @@ const Bio = () => {
   // Set these values by editing "siteMetadata" in gatsby-config.js
   const author = data.site.siteMetadata?.author
   const social = data.site.siteMetadata?.social
+  console.log(social);
 
   return (
     <div className="bio">
@@ -46,8 +47,8 @@ const Bio = () => {
         <p>
           Written by <strong>{author.name}</strong> {author?.summary || null}
           {` `}
-          <a href={`https://twitter.com/${social?.twitter || ``}`}>
-            You should follow them on Twitter
+          <a href={`https://github.com/${social?.twitter || ``}`}>
+            You should follow them on Github
           </a>
         </p>
       )}
